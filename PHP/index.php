63ce3799d975587c1881b0d8bad1e102c092292c
Rhitika Shrestha <rhitikashrestha18@gmail.com>
2024-02-16 20:11:45 +0545

    Testing login page

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,29 @@
-<?php
- <h1> Hello
-?>
+<DOCTYPE html>
+<html>
+    <head> 
+            <meta charset = "UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="=IE=edge" />
+            <meta name=" viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="style.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+            <title> Login Page </title>
+        </head>
+    <body>
+            <div class="login-section">
+               <!-- <div id="login-title"> FEE MANAGEMENT SYSTEM </div>-->
+
+                <form action=" " method="post">
+                    <input class="login-content" type="text" name="" placeholder="User Name"><br>
+                    <input class="login-content" type="password" name="" placeholder="User Password"><br>
+                    <select class="login-content">
+                        <option value="student"> Student </option>
+                        <option value="admin"> Admin </option>
+                    </select><br>
+                    <button id="login-button" type="submit"> Login </button>
+
+                </form>
+            </div>
+          
+    </body>
+</html>
+

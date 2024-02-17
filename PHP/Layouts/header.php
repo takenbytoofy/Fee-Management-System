3709b00a48e6215370b324ea68b5c96fb0c5f354
takenbytoofy <ezalsujakhu312@gmail.com>
2024-02-17 10:51:26 +0545

    Reorganizing files

--- PHP/Layouts/header.php
+++ PHP/Layouts/header.php
@@ -4,6 +4,7 @@
         <title><?php echo $pageTitle??'Fee Management System' ?> </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" link="./CSS/style.css">
     </head>
 
     <body>

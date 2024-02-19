67c951e6b2affcf12c0b1435daca57d81cbd056d
takenbytoofy <ezalsujakhu312@gmail.com>
2024-02-19 13:58:10 +0545

    Progress in the index page

--- Layouts/header.php
+++ Layouts/header.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
 
-    <body>
+    <body style="margin: 0;">
         <style>
 
             #page-container {
--- index.php
+++ index.php
@@ -22,6 +22,37 @@
         background-color: #0e36ad;
     }
 
+    #index-page-nav-header {
+        display: flex;
+        float: left;
+        justify-content: center;
+        align-items: center;
+        width: 60%;
+        height: 50px;
+        color: white;
+        font-size: 32px;
+    }
+
+    #index-page-login-button {
+        display: flex;
+        float: right;
+        justify-content: center;
+        align-items: center;
+
+        width: 100px;
+        height: 50px;
+
+        border: none;
+
+        color: white;
+        font-size: 24px;
+        background-color: #2a4bad;
+    }
+
+    #index-page-login-button:hover {
+        background-color: #1d357a;
+    }
+
 </style>
 
 <div id="index-page-container">
@@ -29,7 +60,8 @@
     <!-- Making an Initial Navigation bar -->
     <div id="index-page-nav-bar">
 
-
+        <span id="index-page-nav-header"> Fee Management System </span>
+        <button id="index-page-login-button" onclick="window.open('./Login/login-page.php','_self')">Login</button>
 
     </div>
 

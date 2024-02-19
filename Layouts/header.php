79c2d26643ee733cb9319ee7b70e7596d88cdf7c
Rhitika Shrestha <rhitikashrestha18@gmail.com>
2024-02-19 19:01:06 +0545

    Adding icon link to header

--- Layouts/header.php
+++ Layouts/header.php
@@ -4,6 +4,7 @@
         <title><?php echo $pageTitle?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     </head>
 
     <body style="margin: 0;">
